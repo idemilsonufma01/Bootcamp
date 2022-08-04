48ce2bc25454fa2a42de9f20f596d703a588adf5
idemilson <idemilson.silva@discente.ufma.br>
2022-08-04 16:54:59 -0300

    Modulo javascript

---
+++ modulo/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Luis', 26));
+console.log(mostraCidade('Idemilson', 'São Paulo'));
+console.log(mostraHobby('Idemilson', 'Ler livros'));
