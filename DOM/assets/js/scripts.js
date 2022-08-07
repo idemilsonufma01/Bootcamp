e29c1f5c6472925854e639799e6d9bcfb5043cd7
idemilson <idemilson.silva@discente.ufma.br>
2022-08-07 19:50:07 -0300

    DOM Javascript

---
+++ DOM/assets/js/scripts.js
@@ -0,0 +1,32 @@
+function changeMode() {
+  changeClasses();
+  changeText();
+}
+
+function changeClasses() {
+  button.classList.toggle(darkModelClass);
+  h1.classList.toggle(darkModelClass);
+  body.classList.toggle(darkModelClass);
+  footer.classList.toggle(darkModelClass);
+}
+
+function changeText() {
+  const lightMode = 'Light Mode';
+  const darkMode = 'Dark Mode';
+
+  if (body.classList.contains(darkModelClass)) {
+    button.innerHTML = lightMode;
+    h1.innerHTML = darkMode + ' ON';
+    return;
+  }
+  button.innerHTML = darkMode;
+  h1.innerHTML = lightMode + ' ON';
+}
+
+const darkModelClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
